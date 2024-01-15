0276f35f1f3f1e82d3c9c8bae59e2e30abc00b12
G-Emmen <greg.emmen@gmail.com>
2024-01-15 16:32:35 -0500

    tab persistance, formatting, project info

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,20 +1,36 @@
 function openSection(evt, sectionName) {
-  // Declare all variables
   var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-
-  // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(sectionName).style.display = "block";
   evt.currentTarget.className += " active";
-} 
+
+  sessionStorage.clear()
+  sessionStorage.setItem("activeTab",sectionName)
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  var selectedTab = sessionStorage.getItem("activeTab");
+  if (selectedTab === "about_me") {
+    document.getElementById("tab1").click();
+  }
+
+  else if (selectedTab === "projects") {
+    document.getElementById("tab2").click();
+  }
+
+  else if (selectedTab === "bonus") {
+    document.getElementById("tab3").click();
+  }
+
+  else {
+    document.getElementById("tab1").click()
+  }
+
+});
